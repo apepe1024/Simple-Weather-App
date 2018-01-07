c798c07cff25bb11e4af6d05bf749209c2f6d346
Alessandro Pepe <apepe1024@gmail.com>
2018-01-06 22:10:22 -0500

    port error

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const router = require("./js/router.js");
 const http = require('http');
 require('dotenv').config();
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 8080
 
 http.createServer(function (request, response) {
   router.home(request, response);
   router.user(request, response);
-}).listen(3000);
-console.log('Server running at Port 3000');
+}).listen(8000);
+console.log('Server running at Port 8000');
