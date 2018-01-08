f8dbaf9b5fb5f6d9ee316c1ff8eacabc18dcbd73
Alessandro Pepe <apepe1024@gmail.com>
2018-01-07 21:12:00 -0500

    IP and port definitions

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const router = require("./js/router.js");
 const http = require('http');
 require('dotenv').config();
 
-var port = process.env.PORT || 8000;
-
+var port = process.env.PORT || 3000;
 http.createServer(function (request, response) {
   router.home(request, response);
   router.user(request, response);
-}).listen(8000);
-console.log('Server running at Port 8000');
+}).listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
+
+
+// var port = process.env.PORT || 8000;
+//
+// .listen(port);
+// console.log('Server running at Port 8000');
